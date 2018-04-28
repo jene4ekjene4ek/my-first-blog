744134efb4c3bccbe23873b7da5cd50f0b38549c
jene4ekjene4ek <eu.bogomaz@gmail.com>
2018-04-28 15:58:23 +0300

    Changed the HTML for thr site

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@=mvccerd#@@!5jm!gcb9lw7$mp1slxox#-ppc4izfjqmlptgl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
